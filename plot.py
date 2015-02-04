fb98082fb324af2fafc2a4fd3f99fc8989723cec
Joe D. Bentley <joebentley10@gmail.com>
2015-02-04 10:44:00 +0000

    Added some changed to plot.py

--- plot.py
+++ plot.py
@@ -7,52 +7,58 @@ import numpy as np
 def parse_file(path, count=False):
     with open(path) as data_file:
         raw = data_file.read().replace('[', '').replace(']', '').split(',')
-        raw = list(map(lambda x: float(x), raw))
-    return raw
-
-
+        return list(map(float, raw))
 
 def main():
     parser = argparse.ArgumentParser(description = 'Generate histogram from Higgs and background data')
-    parser.add_argument('--not_comb', action = 'store_false', help='Do not plot the combined data')
+    parser.add_argument('--not_comb', action = 'store_true', help='Do not plot the combined data')
     parser.add_argument('--higgs', action = 'store_true', help = 'Plots the histogram frmo only the Higgs signal')
     parser.add_argument('--bkg', action = 'store_true', help = 'Plots the histogram from only the background')
     parser.add_argument('--norm', action = 'store_true', help = 'Normalise all plots')
     args = parser.parse_args()
+
     cs_higgs = 17.35
     bf_yy = 2.28e-3
     cs_bkg = 140
-    w_higgs =(cs_higgs * bf_yy)**-1
-    w_bkg = 1/float(cs_bkg)
+    w_higgs = cs_higgs * bf_yy
+    w_bkg = cs_bkg
+
+    print('Higgs weighting:', w_higgs)
+    print('Backr weighting:', w_bkg)
+
     invariant_masses_higgs = parse_file('outputIM_Higgs.txt')
     invariant_masses_bkg = parse_file('outputIM_bkg.txt')
     invariant_masses_combined = parse_file('outputIM_cmb.txt')
+
     #Resolution of histogram
     res = 3
     #Use numpy to manually set up each histogram so the weights can be applied to each and recombined at the end.
     bins = list(map(lambda x: res * x, range(0, int(sorted(invariant_masses_combined)[-1]))))
     hist_higgs, bins = np.histogram(invariant_masses_higgs, bins)
     hist_bkg, bins = np.histogram(invariant_masses_bkg, bins)
-    sum_higgs = float(sum(hist_higgs))
-    sum_bkg = float(sum(hist_bkg))
+
     hist_higgs = list(map(lambda x: x*w_higgs, hist_higgs))
     hist_bkg = list(map(lambda x: x*w_bkg, hist_bkg))
     hist_comb = list(map(lambda x, y: x + y, hist_higgs, hist_bkg))
+
     bins = bins[0:len(bins) - 1]
-    sum_comb = float(sum(hist_comb))
     #hist_comb = list(map(lambda x: x/sum_hist_comb, hist_comb))
     if args.norm:
+        sum_higgs = float(sum(hist_higgs))
+        sum_bkg = float(sum(hist_bkg))
+        sum_comb = float(sum(hist_comb))
         hist_higgs = list(map(lambda x: x/sum_higgs, hist_higgs))
         hist_bkg = list(map(lambda x: x/sum_bkg, hist_bkg))
         hist_comb = list(map(lambda x: x/sum_comb, hist_comb))
-    if args.not_comb:
-        plt.bar(bins, hist_comb, label = 'Higgs + Background')
+
+    if not args.not_comb:
+        plt.bar(bins, hist_comb, label = 'Higgs + Background', color='b')
 
     if args.higgs:
-        plt.bar(bins, hist_higgs, label = 'Higgs')
+        plt.bar(bins, hist_higgs, label = 'Higgs', color='r')
 
     if args.bkg:
-        plt.bar(bins, hist_bkg, label = 'Background')
+        plt.bar(bins, hist_bkg, label = 'Background', color='g')
 
     plt.xlabel('$m_{\gamma \gamma}$ (GeV/$c^2$)')
     plt.ylabel('Frequency')
