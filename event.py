49fe0e5404ef8a84b2efcd6d5b13d7c00d758009
jakelane137 <jakelane137@gmail.com>
2015-02-04 11:49:03 +0000

    Update event.py
    Filtering by 2 angles is now done algorithmically.

--- event.py
+++ event.py
@@ -2,8 +2,7 @@ from fourmomentum import FourMomentum
 from math import *
 
 class Event:
-    def __init__(self, id=0, momenta=None):
-        self.id = id
+    def __init__(self, momenta=None):
         self.momenta = momenta or []
 
     def __str__(self):
@@ -30,9 +29,13 @@ class Event:
                     diff.append(abs(self.momenta[i].azimuthal() - self.momenta[j].azimuthal()))
 
 
-        table = [[0, 1],[0, 2],[1,2],[0,3],[1,3],[2,3],[0,4], [1,4], [2,4],[3,4],[0,5],[1,5],[2,5], [3,5], [4,5], [0,6],[1,6],[2,6],[3,6]]
+        table = []
+        for i in range(0, 50):
+            for j in range(0, 50):
+                if i-j>=1:
+                    table.append([j,i])
         smallest = sorted(diff)[0]
-        print(diff.index(smallest))
+        #print(diff.index(smallest))
         ind = table[diff.index(smallest)]
         momenta = []
         for i in ind:
@@ -48,9 +51,9 @@ class Event:
         return sqrt(abs(b))
 
     @staticmethod
-    def from_text(rest_of_file):
-        """ Generate Event from looking through the rest of the file
-            until another event is found. """
+    def from_text(four_momenta_lines):
+        """ Generate Event from looking through the string given
+            and turning each line of the string into a four momenta. """
 
         event = Event()
 
@@ -58,15 +61,7 @@ class Event:
         momenta = []
 
         # Start looping through the rest of the lines
-        for line_num, momentum_line in enumerate(rest_of_file):
-            # If the line starts with Event and we aren't on line 1, stop parsing
-            if momentum_line.startswith('Event'):
-                if line_num == 0:
-                    event.id = momentum_line.split()[1]
-                    continue
-                else:
-                    break
-
+        for momentum_line in four_momenta_lines:
             # Ignore empty lines
             if len(momentum_line.strip()) == 0:
                 continue
