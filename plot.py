4ee73a6cb2e12298e93a7afbf81c4526ed5fcd0e
jakelane137 <jakelane137@gmail.com>
2015-02-04 02:18:33 +0000

    Changed plotting program
    Manually calculates the histogram so that weights for Higgs signal and background signal can be applied and then added together for the final plot. I removed the parsing arguments from the function in this commit, I may add them later.

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import sys, argparse
+import numpy as np
 
 def parse_file(path, count=False):
     with open(path) as data_file:
@@ -10,35 +11,34 @@ def parse_file(path, count=False):
 
 
 def main():
-    parser = argparse.ArgumentParser(description = 'Generate histogram')
-    parser.add_argument('--higgs_only', action = 'store_true', help = 'Only plot higgs')
-    parser.add_argument('--bkg_only', action = 'store_true', help = 'Only plot background')
-    parser.add_argument('--combined', action = 'store_false', help = 'Default: plot both higgs and background''')
-    args = parser.parse_args()
     cs_higgs = 17.35
     bf_yy = 2.28e-3
     cs_bkg = 140
-    w = cs_higgs * bf_yy/cs_bkg
+    w_higgs =(cs_higgs * bf_yy)**-1
+    w_bkg = 1/float(cs_bkg)
     invariant_masses_higgs = parse_file('outputIM_Higgs.txt')
     invariant_masses_bkg = parse_file('outputIM_bkg.txt')
     invariant_masses_combined = parse_file('outputIM_cmb.txt')
-    weights = list(map(lambda x: w, invariant_masses_higgs))
-    weights_bkg = list(map(lambda x: 1, invariant_masses_bkg))
-    weights_combined = list(map(lambda x: 1, invariant_masses_combined))
-    if args.higgs_only:
-        args.combined = False
-        n, bins, patches = plt.hist(invariant_masses_higgs, 1000, normed=True, weights=weights, facecolor='b', alpha=0.75, label='Higgs')
-    if args.bkg_only:
-        args.combined = False
-        n, bins, patches = plt.hist(invariant_masses_bkg, 1000, normed=True, weights = weights_bkg, facecolor='g', alpha=0.75, label='Background')
-    if args.combined:
-        n, bins, patches = plt.hist(invariant_masses_combined, 1000, normed = True, weights = weights_combined,
-                                facecolor = 'r', alpha = 0.75, label = 'Combined')
-
+    #Resolution of histogram
+    res = 3
+    #Use numpy to manually set up each histogram so the weights can be applied to each and recombined at the end.
+    bins = list(map(lambda x: res * x, range(0, int(sorted(invariant_masses_combined)[-1]))))
+    hist_higgs, bins = np.histogram(invariant_masses_higgs, bins)
+    hist_bkg, bins = np.histogram(invariant_masses_bkg, bins)
+    sum_higgs = float(sum(hist_higgs))
+    sum_bkg = float(sum(hist_bkg))
+    hist_higgs = list(map(lambda x: x*w_higgs, hist_higgs))
+    hist_bkg = list(map(lambda x: x*w_bkg, hist_bkg))
+    hist_comb = list(map(lambda x, y: x + y, hist_higgs, hist_bkg)) 
+    
+    sum_hist_comb = float(sum(hist_comb))
+    #hist_comb = list(map(lambda x: x/sum_hist_comb, hist_comb))
+    plt.bar(bins[0:len(bins) - 1], hist_comb)
+    #plt.bar(bins[0:len(bins) - 1], hist_higgs)
     plt.xlabel('Invariant Mass (GeV/c^2)')
     plt.ylabel('Frequency')
     plt.title('Histogram of invariant masses')
-    plt.axis([0, 200, 0, 0.1])
+    #plt.axis([0, 200, 0, 0.1])
     plt.grid(True)
     plt.legend(loc = 'upper right')
     plt.show()
