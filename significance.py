7c2a7f035247cddd617ecf4edfa32907f538aed1
Jake Lane <jakelane137@gmail.com>
2015-02-25 00:58:09 +0000

    Changed the optimisation plot

--- significance.py
+++ significance.py
@@ -40,6 +40,7 @@ def main():
     parser.add_argument('--invmass', action = 'store_true', help = 'use invariant mass filter')
     parser.add_argument('--plot', action = 'store_true', help = 'plots the optimisation')
     parser.add_argument('--nopreset', action = 'store_false', help = 'wipes all optimisation data')
+    parser.add_argument('--choose_range', action = 'store_false', help = 'takes the old optimised value (from a previous run) and looks 2 parameters (at a chosen resolution)')
     args = parser.parse_args()
     xlabel = ''
     ylabel = ''
@@ -55,7 +56,8 @@ def main():
     higgs_events = parse.parse_file('higgs.txt', momenta_in_event=True)
     if not args.invmass:
         higgs_events = parse.invmass_threshold(higgs_events, m)
-
+    
+    res = 0.05
     filtered_higgs = {}
     default_param = [0, 0, 0, 0, 0, 0, 20]
     opt_p_T1, opt_p_T2, opt_E_1, opt_E_2, opt_dphi, opt_deta ,m = default_param
@@ -64,6 +66,8 @@ def main():
         param = open('optimised.txt', 'r').read().split(',')
         param = list(map(lambda x: float(x), param))
         opt_p_T1, opt_p_T2, opt_E_1, opt_E_2, opt_dphi, opt_deta, m = param
+    
+            
 
     if args.back:
         bkg_events = parse.parse_file('background.txt', momenta_in_event=True)
@@ -71,7 +75,7 @@ def main():
             bkg_events = parse.invmass_threshold(bkg_events, m)
         filtered_bkg = {}
 
-    if args.transverse:
+    if args.transverse: 
     # Apply a series of different filters in turn
         lower_momentum = range(xrange[0], xrange[1], xrange[2])
         higher_momentum = range(yrange[0], yrange[1], yrange[2])
